a465f87eaa99c394abb2e80cef968ae3f1e770e1
Дима Свищев <dima.svishchev01@gmai.com>
2026-06-03 23:40:22 +0300

    сделал тесты по Ingredient(забыл сохранить в прошлый раз)

--- test_recipes.py
+++ test_recipes.py
@@ -1,1 +1,33 @@
 import pytest
+from Ingredients import Ingredient,Recipe, ShoppingList
+
+class Testgenerate:
+    
+    def first(self):
+        ing = Ingredient("сосиска", 500.0, "г")
+        assert ing.name == "мука"
+        assert ing.quantity == 500.0
+        assert ing.unit == "г"
+        
+    def second(self):
+        ing = Ingredient("сосиска", 500.0, "г")
+        assert str(ing) == "'сосиска', 500.0, 'г'"
+        
+    def eq_first(self):
+        ing1  = Ingredient("сосиска", 500.0, "г")
+        ing2 = Ingredient("сосиска", 500.0, "г")
+        assert ing1 == ing2
+        
+    def eq_second(self):
+        ing1  = Ingredient("сосиска", 500.0, "г")
+        ing2 = Ingredient("yabloko", 500.0, "г")
+        assert ing1 != ing2
+        
+    def eq_third(self):
+        ing1  = Ingredient("сосиска", 500.0, "t")
+        ing2 = Ingredient("yabloko", 500.0, "г")
+        assert ing1 != ing2
+        
+    
+        
+        
